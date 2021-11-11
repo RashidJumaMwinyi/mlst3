da67ec095db86a57d5deaa32040254659bb0dd91
Rj12-art <rashj40@gmail.com>
2021-11-11 09:30:06 +0000

    Added the post method for the category

--- run.py
+++ run.py
@@ -157,6 +157,14 @@ def get_categories():
 
 @app.route("/add_category", methods = ["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name" : request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New Category Added")
+        return redirect(url_for("get_categories"))
+
     return render_template("add_category.html")
 
 
